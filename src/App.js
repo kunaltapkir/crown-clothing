eb3633870dd0b2a5535b7117bcbd858df1f7b621
Kunal Tapkir <kunal.tapkir@synerzip.com>
2020-05-25 12:42:31 +0530

    Added Custom FormInput component and custom Button component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePageComponent";
 import ShopPage from "./pages/Shop/ShopComponent";
 import Header from "./components/Header/HeaderComponent";
+import SignInAndSignUpPage from "./pages/SignIn-and-SignUp/SignIn-and-SignUp";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/SignIn-and-SignUp/SignIn-and-SignUp.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./SignIn-and-SignUp.styles.scss";
+import SignIn from "../../components/Sign-In/Sign-In-Component";
+
+const SignInAndSignUpPage = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUpPage;
